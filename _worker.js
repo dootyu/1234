b00a4a03a3469b6db2ca5a408205158267db6f85
dootyu <108780848+dootyu@users.noreply.github.com>
2022-08-08 03:07:37 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'dfhhhgfhf.herokuapp.com'
+      url.hostname = 'xx.xx.xx'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
